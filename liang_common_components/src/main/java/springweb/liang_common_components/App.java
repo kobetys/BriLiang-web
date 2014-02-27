697c10788c968f57968bc915af88f06386135776
liangwen.liang <liangwen.liang@vipshop.com>
2014-02-27 15:40:32 +0800

    add new java file

---
+++ liang_common_components/src/main/java/springweb/liang_common_components/App.java
@@ -0,0 +1,13 @@
+package springweb.liang_common_components;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
